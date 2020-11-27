74129df0f0d712fd22f08db6db2114a0fa47603a
hipi <yuyehack@gmail.com>
2020-11-27 15:08:51 +0800

    pwa

--- vite.config.js
+++ vite.config.js
@@ -32,6 +32,11 @@ export default {
           },
         ],
       },
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true,
+        cleanupOutdatedCaches: true,
+      },
     }),
   ],
 };
