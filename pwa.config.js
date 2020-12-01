f5adf1d682388cab74879e14dc6492bdf2a1c117
hipi <yuyehack@gmail.com>
2020-12-01 17:51:54 +0800

    pwa-my

--- pwa.config.js
+++ pwa.config.js
@@ -30,7 +30,7 @@ module.exports = {
       },
     ],
   },
-  pworkbox: {
+  workbox: {
     clientsClaim: true,
     skipWaiting: true,
     cleanupOutdatedCaches: true,
--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,15 @@
-export default {};
+function aaa() {
+  console.log(1111111111);
+}
+export default {
+  configureBuild: function () {
+    console.log(2222);
+  },
+  plugins: [
+    {
+      rollupInputOptions: {
+        pluginsOptimizer: [aaa()],
+      },
+    },
+  ],
+};
