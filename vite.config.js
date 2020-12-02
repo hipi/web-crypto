2867397810490d7d261ee4d4f3b122810a554ea0
hipi <yuyehack@gmail.com>
2020-12-02 14:29:50 +0800

    publish

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,1 @@
-function aaa() {
-  console.log(1111111111);
-}
-export default {
-  configureBuild: function () {
-    console.log(2222);
-  },
-  plugins: [
-    {
-      rollupInputOptions: {
-        pluginsOptimizer: [aaa()],
-      },
-    },
-  ],
-};
+export default {};
